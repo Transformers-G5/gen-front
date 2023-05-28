2cdf2f02f9680759a47cfbedaab69c803a039d37
RG-404 <55778353+RG-404@users.noreply.github.com>
2023-05-29 02:06:54 +0530

    integration with blog api

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import WorkspaceLayout from "./layout/Workspace";
+import Test from "./components/Test";
+import Gallery from "./screen/Gallery";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<WorkspaceLayout />}>
+          <Route index element={<Test />} />
+          <Route path="gallery" element={<Gallery />} />
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
